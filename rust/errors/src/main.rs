110cc23ceffde3285baf003c6553a5ebf85ae74a
Vlad Riscutia <vladris@microsoft.com>
2018-02-16 07:31:52 -0800

    Errors

--- rust/errors/src/main.rs
+++ rust/errors/src/main.rs
@@ -1,3 +1,62 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    panic!("Crash and burn");
+    /*
+    Cause panic!
+
+    let v = vec![1, 2, 3];
+
+    v[99];
+    */
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("Can't create file: {:?}", e)
+                },
+            }
+        },
+        Err(error) => {
+            panic!("Problem opening file: {:?}", error)
+        },
+    };
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
 }
