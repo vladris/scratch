7761d5c6002a441015d4d4224e8249beb8a89982
Vlad Riscutia <vladris@outlook.com>
2018-09-24 14:25:57 -0700

    Initial commit

---
+++ taomp/taomp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace taomp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
